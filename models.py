b4a48f700ff94c38d005fe9b5e34c813282a4725
Janengethe <janengethew@gmail.com>
2022-09-22 10:58:51 +0300

    initial commit

---
+++ models.py
@@ -0,0 +1,28 @@
+from app import db
+
+class Book(db.Model):
+    __tablename__ = 'books'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    published = db.Column(db.String())
+
+    def __init__(self, name, author, published):
+        self.name = name
+        self.author = author
+        self.published = published
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+    
+    
+    # Note that serialize method here is not needed for database migration but
+    # it will be useful when we need to return book objects in response as JSON.
+    def serialize(self):
+        return {
+            'id': self.id, 
+            'name': self.name,
+            'author': self.author,
+            'published':self.published
+        }
